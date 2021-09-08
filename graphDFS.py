0be515a5f0af492b8ad0b4e9605169cee3013e9e
krishnacse2010 <krishcse20@gmail.com>
2021-09-08 07:37:48 +0530

    Graph DFS Algo

--- graphDFS.py
+++ graphDFS.py
@@ -6,15 +6,15 @@ class Graph:
         self.graph = defaultdict(list) ;#Pass callable , so not list()
 
     def addEdge(self,v,e):
-        print(f'Adding to Vertex{v} edge{e}',self.graph[v])
+        # For undirected graph we need to add both v <-> e
         self.graph[v].append(e)
         self.graph[e].append(v)
 
     def DFS(self,s,seenVertex):
-
+        #add vertex
         seenVertex.add(s)
-        print(s)
-        #print(self.graph[s])
+        
+        #Call recursively the neighbours and add if not already added using the set
         for elem in self.graph[s]:
             if elem not in seenVertex:
                 self.DFS(elem,seenVertex)
@@ -22,7 +22,7 @@ class Graph:
 
 
 
-g = Graph()
+
 g = Graph()
 g.addEdge(0, 1)
 g.addEdge(0, 2)
@@ -31,4 +31,5 @@ g.addEdge(2, 0)
 g.addEdge(2, 3)
 g.addEdge(3, 3)
 
+# Using set DS to ensure vertex is added only once
 print(g.DFS(2,set()))
